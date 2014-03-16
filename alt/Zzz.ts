8d8e5e5d4f9fc34d61ae877247beae8c9ac0ec77
uupaa <uupaa.js@gmail.com>
2014-03-17 06:19:21 +0900

    improve alt/Zzz.ts and TypeScript settings

--- alt/Zzz.ts
+++ alt/Zzz.ts
@@ -2,16 +2,22 @@
 
 export class Zzz {
     static name = "Zzz";
-    static repository = "https://github.com/uupaa/Zzz.js/";
+    static repository = "https://github.com/uupaa/Zzz.js";
 
-    _arg: any;
+    _value: number;
 
-    constructor(arg:String)
-    constructor(arg:Number)
-    constructor(arg:any) {
+    constructor(value:number) {
+        this._value = value;
     }
-    method(arg:any):Zzz {
-        return this;
+    value():number {
+        return this._value;
+    }
+    isNumber():boolean {
+        return typeof this._value === "number";
+    }
+    isInteger():boolean {
+        return typeof this._value === "number" &&
+               Math.ceil(this._value) === this._value;
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 new Test().add([
-        testZzz,
+        testZzz_value,
+        testZzz_isNumber,
+        testZzz_isInteger,
     ]).run(function(err, test) {
         if (1) {
             err || test.worker(function(err, test) {
@@ -14,14 +16,48 @@ new Test().add([
         }
     });
 
-function testZzz(next) {
+function testZzz_value(next) {
 
-    if (true) {
-        console.log("testXxx ok");
-        next && next.pass();
+    var result = new Zzz(123.4).value();
+
+    if (result !== 123.4) {
+        console.log("testZzz_value ng");
+        next && next.miss(); // see Task.js
     } else {
-        console.log("testXxx ng");
+        console.log("testZzz_value ok");
+        next && next.pass(); // see Task.js
+    }
+}
+
+function testZzz_isNumber(next) {
+
+    var result = [
+            new Zzz(123.4).isNumber(),  // true
+            new Zzz(123.0).isNumber()   // true
+        ];
+
+    if (/false/.test(result.join())) {
+        console.log("testZzz_isNumber ng");
         next && next.miss();
+    } else {
+        console.log("testZzz_isNumber ok");
+        next && next.pass();
+    }
+}
+
+function testZzz_isInteger(next) {
+
+    var result = [
+           !new Zzz(123.4).isInteger(), // !false -> true
+            new Zzz(123.0).isInteger()  // true
+        ];
+
+    if (/false/.test(result.join())) {
+        console.log("testZzz_isInteger ng");
+        next && next.miss();
+    } else {
+        console.log("testZzz_isInteger ok");
+        next && next.pass();
     }
 }
 
