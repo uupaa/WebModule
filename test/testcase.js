d99dd38e0a41b123537105060c9b42a3eed1a8c5
uupaa <uupaa.js@gmail.com>
2015-01-09 21:30:27 +0900

    add Test.toHex, Test.likeArray, Test.likeObject

--- test/testcase.js
+++ test/testcase.js
@@ -1,7 +1,7 @@
 var ModuleTestREPOSITORY_NAME = (function(global) {
 
 var _isNodeOrNodeWebKit = !!global.global;
-var _runOnNodeWebKit =  _isNodeOrNodeWebKit && /native/.test(setTimeout);
+var _runOnNodeWebKit =  _isNodeOrNodeWebKit &&  /native/.test(setTimeout);
 var _runOnNode       =  _isNodeOrNodeWebKit && !/native/.test(setTimeout);
 var _runOnWorker     = !_isNodeOrNodeWebKit && "WorkerLocation" in global;
 var _runOnBrowser    = !_isNodeOrNodeWebKit && "document" in global;
