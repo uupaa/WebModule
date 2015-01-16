371cda2ee43ecf609957722bbcf54d6ac230b320
uupaa <uupaa.js@gmail.com>
2015-01-16 14:21:33 +0900

    improve test case template

--- test/testcase.js
+++ test/testcase.js
@@ -6,11 +6,12 @@ var _runOnNode       =  _isNodeOrNodeWebKit && !/native/.test(setTimeout);
 var _runOnWorker     = !_isNodeOrNodeWebKit && "WorkerLocation" in global;
 var _runOnBrowser    = !_isNodeOrNodeWebKit && "document" in global;
 
-return new Test("REPOSITORY_NAME", {
+var test = new Test("REPOSITORY_NAME", {
         disable:    false,
         browser:    true,
         worker:     true,
         node:       true,
+        nw:         true,
         button:     true,
         both:       true, // test the primary module and secondary module
         ignoreError:false,
@@ -18,7 +19,17 @@ return new Test("REPOSITORY_NAME", {
         testREPOSITORY_NAME_value,
         testREPOSITORY_NAME_isNumber,
         testREPOSITORY_NAME_isInteger,
-    ]).run().clone();
+    ]);
+
+if (_runOnBrowser || _runOnNodeWebKit) {
+    //test.add([]);
+} else if (_runOnWorker) {
+    //test.add([]);
+} else if (_runOnNode) {
+    //test.add([]);
+}
+
+return test.run().clone();
 
 function testREPOSITORY_NAME_value(test, pass, miss) {
 
@@ -33,29 +44,29 @@ function testREPOSITORY_NAME_value(test, pass, miss) {
 
 function testREPOSITORY_NAME_isNumber(test, pass, miss) {
 
-    var result = [
-            new REPOSITORY_NAME(123.4).isNumber(),  // true
-            new REPOSITORY_NAME(123.0).isNumber()   // true
-        ];
+    var result = {
+            0: new REPOSITORY_NAME(123.4).isNumber(),  // true
+            1: new REPOSITORY_NAME(123.0).isNumber()   // true
+        };
 
-    if (!/false/.test(result.join())) {
-        test.done(pass());
-    } else {
+    if (/false/.test(JSON.stringify(result, null, 2))) {
         test.done(miss());
+    } else {
+        test.done(pass());
     }
 }
 
 function testREPOSITORY_NAME_isInteger(test, pass, miss) {
 
-    var result = [
-           !new REPOSITORY_NAME(123.4).isInteger(), // !false -> true
-            new REPOSITORY_NAME(123.0).isInteger()  // true
-        ];
+    var result = {
+            0: !new REPOSITORY_NAME(123.4).isInteger(), // (!false) -> true
+            1:  new REPOSITORY_NAME(123.0).isInteger()  // true
+        };
 
-    if (!/false/.test(result.join())) {
-        test.done(pass());
-    } else {
+    if (/false/.test(JSON.stringify(result, null, 2))) {
         test.done(miss());
+    } else {
+        test.done(pass());
     }
 }
 
