12a16ed208646223fac14c6ad53985ca86b85c73
uupaa <uupaa.js@gmail.com>
2015-06-05 21:54:39 +0900

    tweak comment

--- BASE_MODEL/lib/WebModule.js
+++ BASE_MODEL/lib/WebModule.js
@@ -1,6 +1,7 @@
 // http://git.io/WebModule
 
 // --- global variables ------------------------------------
+// https://github.com/uupaa/WebModule/wiki/WebModuleIdiom
 var GLOBAL = GLOBAL || (this || 0).self || global;
 
 // --- environment detection -------------------------------
@@ -11,13 +12,12 @@ GLOBAL.IN_NODE       =  GLOBAL.IN_NODE_OR_NW && !/native/.test(setTimeout);
 GLOBAL.IN_NW         =  GLOBAL.IN_NODE_OR_NW &&  /native/.test(setTimeout);
 
 // --- validate and assert functions -----------------------
-//{@dev https://github.com/uupaa/WebModule/wiki/Validate
-GLOBAL["$type"]  = function(value, types)                 { return GLOBAL["Valid"] ? GLOBAL["Valid"].type(value, types) : true; };
-GLOBAL["$keys"]  = function(value, keys)                  { return GLOBAL["Valid"] ? GLOBAL["Valid"].keys(value, keys)  : true; };
-GLOBAL["$some"]  = function(value, candidate, ignoreCase) { return GLOBAL["Valid"] ? GLOBAL["Valid"].some(value, candidate, ignoreCase) : true; };
-GLOBAL["$args"]  = function(api, args)                    { if (GLOBAL["Valid"]) { GLOBAL["Valid"].args(api, args); } };
-GLOBAL["$valid"] = function(value, api, highlihgt)        { if (GLOBAL["Valid"]) { GLOBAL["Valid"](value, api, highlihgt); } };
-//}@dev
+// https://github.com/uupaa/WebModule/wiki/Validate
+GLOBAL.$type  = function(value, types)                 { return GLOBAL["Valid"] ? GLOBAL["Valid"].type(value, types) : true; };
+GLOBAL.$keys  = function(value, keys)                  { return GLOBAL["Valid"] ? GLOBAL["Valid"].keys(value, keys)  : true; };
+GLOBAL.$some  = function(value, candidate, ignoreCase) { return GLOBAL["Valid"] ? GLOBAL["Valid"].some(value, candidate, ignoreCase) : true; };
+GLOBAL.$args  = function(api, args)                    { if (GLOBAL["Valid"]) { GLOBAL["Valid"].args(api, args); } };
+GLOBAL.$valid = function(value, api, highlihgt)        { if (GLOBAL["Valid"]) { GLOBAL["Valid"](value, api, highlihgt); } };
 
 // --- WebModule ------------------------------------------
 GLOBAL.WebModule = {
