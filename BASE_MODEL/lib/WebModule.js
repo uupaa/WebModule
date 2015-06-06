de1d86cb62c43776f8cce87224808e04ada997be
uupaa <uupaa.js@gmail.com>
2015-06-07 07:29:22 +0900

    fix Help()

--- BASE_MODEL/lib/WebModule.js
+++ BASE_MODEL/lib/WebModule.js
@@ -5,24 +5,25 @@
 var GLOBAL = GLOBAL || (this || 0).self || global;
 
 // --- environment detection -------------------------------
-GLOBAL.IN_NODE_OR_NW = !!GLOBAL.global;
-GLOBAL.IN_BROWSER    = !GLOBAL.IN_NODE_OR_NW && "document" in GLOBAL;
-GLOBAL.IN_WORKER     = !GLOBAL.IN_NODE_OR_NW && "WorkerLocation" in GLOBAL;
-GLOBAL.IN_NODE       =  GLOBAL.IN_NODE_OR_NW && !/native/.test(setTimeout);
-GLOBAL.IN_NW         =  GLOBAL.IN_NODE_OR_NW &&  /native/.test(setTimeout);
+GLOBAL["IN_NODE_OR_NW"] = !!GLOBAL.global;
+GLOBAL["IN_BROWSER"]    = !GLOBAL["IN_NODE_OR_NW"] && "document" in GLOBAL;
+GLOBAL["IN_WORKER"]     = !GLOBAL["IN_NODE_OR_NW"] && "WorkerLocation" in GLOBAL;
+GLOBAL["IN_NODE"]       =  GLOBAL["IN_NODE_OR_NW"] && !/native/.test(setTimeout);
+GLOBAL["IN_NW"]         =  GLOBAL["IN_NODE_OR_NW"] &&  /native/.test(setTimeout);
 
 // --- validate and assert functions -----------------------
-// https://github.com/uupaa/WebModule/wiki/Validate
-GLOBAL.$type  = function(value, types)                 { return GLOBAL["Valid"] ? GLOBAL["Valid"].type(value, types) : true; };
-GLOBAL.$keys  = function(value, keys)                  { return GLOBAL["Valid"] ? GLOBAL["Valid"].keys(value, keys)  : true; };
-GLOBAL.$some  = function(value, candidate, ignoreCase) { return GLOBAL["Valid"] ? GLOBAL["Valid"].some(value, candidate, ignoreCase) : true; };
-GLOBAL.$args  = function(api, args)                    { if (GLOBAL["Valid"]) { GLOBAL["Valid"].args(api, args); } };
-GLOBAL.$valid = function(value, api, highlihgt)        { if (GLOBAL["Valid"]) { GLOBAL["Valid"](value, api, highlihgt); } };
+//{@dev https://github.com/uupaa/WebModule/wiki/Validate
+GLOBAL["$type"]  = function(value, types)                 { return GLOBAL["Valid"] ? GLOBAL["Valid"].type(value, types) : true; };
+GLOBAL["$keys"]  = function(value, keys)                  { return GLOBAL["Valid"] ? GLOBAL["Valid"].keys(value, keys)  : true; };
+GLOBAL["$some"]  = function(value, candidate, ignoreCase) { return GLOBAL["Valid"] ? GLOBAL["Valid"].some(value, candidate, ignoreCase) : true; };
+GLOBAL["$args"]  = function(api, args)                    { if (GLOBAL["Valid"]) { GLOBAL["Valid"].args(api, args); } };
+GLOBAL["$valid"] = function(value, api, highlihgt)        { if (GLOBAL["Valid"]) { GLOBAL["Valid"](value, api, highlihgt); } };
+//}@dev
 
 // --- WebModule ------------------------------------------
 GLOBAL.WebModule = {
     exports: function(name, closure) {
-        var alias = name in GLOBAL ? (name + "_") : name;
+        var alias = name in GLOBAL["WebModule"] ? (name + "_") : name;
         var entity = alias in this ? this[alias]
                                    : this[alias] = closure(GLOBAL);
 
