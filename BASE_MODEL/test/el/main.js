87649f4eef6f2c108cc0aa9fe0052b4af8dc807b
uupaa <uupaa.js@gmail.com>
2015-11-26 16:52:33 +0900

    fix relationship to working directory.

--- BASE_MODEL/test/el/main.js
+++ BASE_MODEL/test/el/main.js
@@ -3,6 +3,10 @@
 var app = require("app");
 var BrowserWindow = require("browser-window");
 
+process.chdir("test/el");
+// var cwd = process.cwd();
+// console.log(cwd);
+
 require("crash-reporter").start();
 
 var mainWindow = null;
@@ -14,9 +18,13 @@ app.on("window-all-closed", function() {
 app.on("ready", function() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
   mainWindow.loadUrl("file://" + __dirname + "/index.html");
+//  mainWindow.webContents.on("did-finish-load", function() {
+//    mainWindow.webContents.executeJavaScript("process.chdir('" + cwd + "')");
+//  });
 
   mainWindow.on("closed", function() {
     mainWindow = null;
   });
 });
+
 
